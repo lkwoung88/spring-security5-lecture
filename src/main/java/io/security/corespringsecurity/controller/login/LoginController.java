dd3f4876105288d0d6b3d7bd35174cd778f3cedd
lkwoung88 <lkwoung88@gmail.com>
2024-03-26 01:00:57 +0900

    feat : 인가 예외처리 구현 - AccessDeniedHandler

--- src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
+++ src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
@@ -1,5 +1,6 @@
 package io.security.corespringsecurity.controller.login;
 
+import io.security.corespringsecurity.domain.Account;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
@@ -35,5 +36,15 @@ public class LoginController {
         }
 
         return "redirect:/login";
+    }
+
+    @GetMapping("/denied")
+    public String accessDenied(@RequestParam(value = "exception", required = false) String exception, Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Account account = (Account) authentication.getPrincipal();
+        model.addAttribute("username", account.getUsername());
+        model.addAttribute("exception", exception);
+
+        return "user/login/denied";
     }
 }
