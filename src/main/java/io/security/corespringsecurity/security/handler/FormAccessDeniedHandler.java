82be827b37c48ca791086bf921ffe2d3112e158d
lkwoung88 <lkwoung88@gmail.com>
2024-04-29 04:11:36 +0900

    feat : DB연동 인증기능 수정 - html 비밀번호 부분 수정 - 오류메시지 수정 (인코딩)

--- src/main/java/io/security/corespringsecurity/security/handler/FormAccessDeniedHandler.java
+++ src/main/java/io/security/corespringsecurity/security/handler/FormAccessDeniedHandler.java
@@ -15,6 +15,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class FormAccessDeniedHandler implements AccessDeniedHandler {
@@ -24,20 +26,22 @@ public class FormAccessDeniedHandler implements AccessDeniedHandler {
 	private ObjectMapper mapper = new ObjectMapper();
 
 	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
-	
+
 	@Override
 	public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+		String encodedMessage = URLEncoder.encode(accessDeniedException.getMessage(), StandardCharsets.UTF_8.toString());
 
 		if (WebUtil.isAjax(request)) {
 			response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 			response.getWriter().write(this.mapper.writeValueAsString(ResponseEntity.status(HttpStatus.FORBIDDEN)));
 
 		} else {
-			String deniedUrl = errorPage + "?exception=" + accessDeniedException.getMessage();
+			String deniedUrl = errorPage + "?exception=" + encodedMessage;
 			redirectStrategy.sendRedirect(request, response, deniedUrl);
 		}
 	}
-	
+
 	public void setErrorPage(String errorPage) {
         if ((errorPage != null) && !errorPage.startsWith("/")) {
             throw new IllegalArgumentException("errorPage must begin with '/'");
