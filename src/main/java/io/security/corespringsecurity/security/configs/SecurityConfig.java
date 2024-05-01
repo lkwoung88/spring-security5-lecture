b8ce828adf6308d436fbcee71b557d2c9acf0f5a
lkwoung88 <lkwoung88@gmail.com>
2024-05-02 02:48:14 +0900

    feat : method 방식 인가기능 구현(2) - DB연동 설정

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -44,7 +44,6 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
